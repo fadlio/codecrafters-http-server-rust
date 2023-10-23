12eec1b340bce770707aa24f54a578f5ed7173a5
Fernando de Lio <fernandodelio@gmail.com>
2023-10-23 12:42:52 -0300

    headers parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::io::{BufRead, Read, Write};
 use std::net::{TcpListener, TcpStream};
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use itertools::Itertools;
 
 enum HttpMethod {
@@ -19,23 +20,41 @@ impl From<&str> for HttpMethod {
     }
 }
 
-struct HttpFrame<'a> {
+struct HttpRequest<'a> {
     method: HttpMethod,
     path: &'a str,
     version: &'a str,
+    headers: HashMap<String, String>,
 }
 
-impl HttpFrame<'_> {
-    fn from_request_str(buffer: &str) -> Result<HttpFrame> {
-        let header = buffer.split("\r\n").next().ok_or(anyhow!("Invalid frame"))?;
-        match header.split(' ').collect_tuple() {
-            Some((method, path, version)) => Ok(HttpFrame {
-                method: HttpMethod::from(method),
-                version,
-                path,
-            }),
-            _ => Err(anyhow!("Invalid frame"))
-        }
+impl HttpRequest<'_> {
+    fn from_request_str(buffer: &str) -> Result<HttpRequest> {
+        let mut lines = buffer.split("\r\n");
+
+        let (method, path, version) = lines
+            .next().ok_or(anyhow!("Invalid frame"))?
+            .split(' ')
+            .collect_tuple().ok_or(anyhow!("Invalid frame"))?;
+
+        let headers: HashMap<_, _> = lines
+            .filter_map(|l| {
+                if let Some((key, value)) = l.split_once(": ") {
+                    Some((
+                        key.to_string(),
+                        value.to_string(),
+                    ))
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        Ok(HttpRequest {
+            method: HttpMethod::from(method),
+            version,
+            path,
+            headers,
+        })
     }
 }
 
@@ -43,21 +62,40 @@ fn send_text_plain(mut stream: &TcpStream, text: &str) -> Result<usize> {
     let mut data = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n".to_string();
     data.push_str(&*format!("Content-Length: {}\r\n\r\n", text.len()));
     data.push_str(text);
-    let bytes_written = stream.write(data.as_bytes())?;
+    let bytes_written = stream.write(data.as_bytes()).context("Failed to send")?;
     Ok(bytes_written)
 }
 
+fn not_found_route(mut stream: &TcpStream) -> Result<usize> {
+    stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").context("Failed to send")
+}
 
-fn handle_connection(mut stream: TcpStream) -> Result<()> {
+fn index_route(mut stream: &TcpStream) -> Result<usize> {
+    stream.write(b"HTTP/1.1 200 OK\r\n\r\n").context("Failed to send")
+}
+
+fn echo_route(stream: &TcpStream, frame: &HttpRequest) -> Result<usize> {
+    send_text_plain(&stream, &frame.path[6..])
+}
+
+fn user_agent_route(stream: &TcpStream, frame: &HttpRequest) -> Result<usize> {
+    let user_agent = frame.headers.get("User-Agent")
+        .ok_or(anyhow!("User Agent not found"))?;
+    send_text_plain(&stream, user_agent.as_str())
+}
+
+
+fn route_connection(mut stream: TcpStream) -> Result<()> {
     let mut buffer: [u8; 512] = [0; 512];
     stream.read(&mut buffer)?;
     let request = std::str::from_utf8(&buffer)?;
-    let frame = HttpFrame::from_request_str(&request)?;
+    let frame = HttpRequest::from_request_str(&request)?;
     match frame.path {
-        "/" => stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?,
-        _ if frame.path.starts_with("/echo/") => send_text_plain(&stream, &frame.path[6..])?,
-        _ => stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?,
-    };
+        "/" => index_route(&stream),
+        _ if frame.path.starts_with("/echo/") => echo_route(&stream, &frame),
+        _ if frame.path.starts_with("/user-agent") => user_agent_route(&stream, &frame),
+        _ => not_found_route(&stream),
+    }?;
 
     Ok(())
 }
@@ -69,7 +107,7 @@ fn main() -> Result<()> {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                handle_connection(stream)?;
+                route_connection(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
